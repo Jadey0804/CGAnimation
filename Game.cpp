5b2e7bf1838bc37ca7478d26e5dd3583c95a1d6b
Jadey0804 <2651466805@qq.com>
2025-12-20 13:48:13 +0000

    groundlandTexture2

--- Game.cpp
+++ Game.cpp
@@ -110,14 +110,11 @@ int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR lpCmdLine, int nC
 		Matrix skyV = v.removeTranslation();
 		Matrix skyVP = skyV * p;
 
-		// ✅ 一定要先开始 RenderPass，再画任何东西
+		
 		core.beginRenderPass();
 
-		// ✅ 更新动画
 		level.update(dt);
 
-		// ✅ 画整个关卡（plane / static / anim / skybox）
-		// time 我建议传 t（累计时间），不要传 dt
 		level.draw(vp, skyVP, t, fpscamera.position);
 
 		core.finishFrame();
